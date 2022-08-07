e48ba55dbe4c92d416b0f65aae7633adde7525cf
prolab773 <prolab773@gmail.com>
2022-08-07 17:29:54 +0000

    init commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const { json } = require('express');
+const express = require('express');
+const app = express();
+
+const tasks = require('./routes/tasks');
+
+// middleware
+
+app.use(express.json())
+
+// routes
+app.get('/hello', (req,res) => {
+    res.send('Task Manager app')
+})
+
+app.use('/api/v1/tasks',tasks)
+
+
+const port = 5000;
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`);
+})
