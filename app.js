f2ec4655c1cf6d1338110c5e7efc08f7085ab3d3
prolab773 <prolab773@gmail.com>
2022-08-10 13:53:14 +0000

    dotenv package lesson

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 // middleware
 
@@ -18,7 +20,13 @@ app.use('/api/v1/tasks',tasks)
 
 const port = 5000;
 
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+    } catch(error){
+        console.log(error);
+    }
+}
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`);
-})
+start()
