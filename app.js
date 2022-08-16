8ed3ffc3a560a7ff0823a6a289a604386adbf621
prolab773 <prolab773@gmail.com>
2022-08-16 00:09:19 +0000

    finish learning Task manager project

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
 app.use(express.static('./public'));
@@ -13,8 +15,10 @@ app.use(express.json())
 // routes
 app.use('/api/v1/tasks',tasks)
 
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
     try{
